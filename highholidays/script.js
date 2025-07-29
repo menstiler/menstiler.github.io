cc1afb846b7dd800037e23ccb5273d30cdd0050b
Mendel Stillerman <menstiler@gmail.com>
2025-07-28 22:36:04 -0400

    updated styles

--- highholidays/script.js
+++ highholidays/script.js
@@ -26,6 +26,8 @@ function injectAsset(rule) {
 PAGE_SCRIPTS.forEach((rule) => {
   if (window.location.href.indexOf(rule.aid) !== -1) {
     injectAsset(rule);
+  } else {
+    localStorage.removeItem("submissionId");
   }
 });
 
@@ -121,9 +123,6 @@ async function init() {
   await getFromSheet();
   window.addEventListener("resize", checkScrollable);
   checkScrollable();
-  window.addEventListener("unload", function () {
-    localStorage.removeItem("scriptHasRun");
-  });
 }
 
 if (document.readyState !== "loading") {
