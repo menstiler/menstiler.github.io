6b98804a6cee1f1a4dd7fd1dfac9cff81a934076
Mendel Stillerman <menstiler@gmail.com>
2025-08-06 13:36:58 -0400

    updating homepage

--- chabadofislip/homepage.js
+++ chabadofislip/homepage.js
@@ -56,6 +56,39 @@ const promosWidgetHtml = `<div class="promos-bg">
     </div>
 </div>`;
 
+const mobileSubscribeWidget = `<form autocomplete="off" class="bottom_subscribe_box" action="/templates/articlecco.asp?aid=4456691" method="post" id="form1" onsubmit="return Co.Forms.Validation.Validate(this);" novalidate="true">
+<input type="hidden" name="site" value="">         
+<input type="hidden" name="sc" value="">
+<div class="co_body article-body cf">
+<div id="formBody">
+<div class="field-row">
+<input type="text" class="mobile_input_field" name="q3_fullName[first]" placeholder="First Name" tabindex="1" required="false" class=" active active">         
+<input type="text" class="mobile_input_field" name="q3_fullName[last]" placeholder="Last Name" tabindex="1" required="false" class=" active active">
+<input type="text" class="mobile_input_field" name="q6_email" placeholder="Email" tabindex="1" required="false" class=" active active">   
+</div>
+<div class="break_floats"> </div>
+<div class="clearfix">
+<script>
+      var recaptchaIsEnterprise = false;
+           var recaptchaV2Key = "6LcG_TcUAAAAAKAVgwgW39ujc9OCjXSoQYFIA-Su";
+  
+  </script>          
+<input type="hidden" class="js-recaptcha-input" name="cdo-captcha-response" value="" data-div-id="aa8b9f8c-fc59-4a59-9cd6-68d3089944e0" data-processed="false">
+<div class="js-recaptcha-wrapper" id="aa8b9f8c-fc59-4a59-9cd6-68d3089944e0"> </div>
+</div>
+<div class="ccoFormFooter">
+<div class="small bold center bottom_padding">
+<div class="grey f-small"> </div>
+</div>
+<div align="center">
+<input type="submit" value="Subscribe" name="SubmitCCO" tabindex="1000" id="CoButton">
+<p align="center" style="font-size: 8pt; color: #666666; font-family: Verdana, Helvetica; margin-bottom: 3px; margin-top: 6px;">
+<img valign="absbottom" src="https://w2.chabad.org/images/global/icons/lock.gif" width="16" height="16" alt="Secure">                                   This page uses 128 bit SSL encryption to keep your data secure.</p>
+</div>
+<input type="hidden" name="FormCCOSubmited" value="true"></div>
+</form>
+`;
+
 function setUpSearch() {
   jQuery("#tabContentMain").append(
     '<div class="search_menu_item search-trigger-container"><a href="#" class="search-trigger"><i class="fa fa-search"></i></a></div>'
@@ -66,6 +99,7 @@ function setUpSearch() {
   jQuery(".search-trigger").on("click", function () {
     jQuery(".cco_search_header").addClass("show");
     jQuery("#topAreaTopSearch_search").focus();
+    jQuery(".search_menu_item").hide();
     return false;
   });
 
@@ -76,6 +110,7 @@ function setUpSearch() {
     ) {
     } else {
       jQuery(".cco_search_header").removeClass("show");
+      jQuery(".search_menu_item").show();
     }
   });
 }
@@ -122,15 +157,18 @@ function init() {
     );
 
     if (subscribeContainer) {
+      const subscribeContainerForm = document.querySelector(
+        "#HeaderSubscribeContainer form"
+      );
+      subscribeContainerForm.classList.remove("subscribe_box");
+      subscribeContainerForm.classList.add("bottom_subscribe_box");
       parentElement.after(subscribeContainer);
     }
 
     parentElement.insertAdjacentHTML("afterend", promosWidgetHtml);
 
     if (jQuery("body").hasClass("mobile") && !subscribeContainer) {
-      const subscribeForm =
-        '<form class="subscribe-form" method="get" action="/subscribe"><button type="submit" id="CoButton" value="Subscribe to our mailing list " name="co_form_submit">Subscribe to our mailing list</button></form>';
-      jQuery(footer).prepend(subscribeForm);
+      jQuery(footer).prepend(mobileSubscribeWidget);
     }
 
     // small promos
